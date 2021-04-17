68dd3ea7593fbb7e652c0b188aadadb477cfc0f4
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-04-16 21:42:17 -0700

    Feat: Created ability to add role.

--- server.js
+++ server.js
@@ -52,12 +52,92 @@ async function viewAllRoles() {
     startApp()
 }
 
-
 // Add ability to view all employees
+async function viewAllEmployees() {
+    const employee = await db.viewAllEmployees();
+    console.log('\n');
+    console.table(employee);
+
+    startApp()
+}
 
 // Add ability to ADD department 
+async function addDepartment() {
+    const department = await inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter department name.',
+            validate: answer => {
+                if (answer === '') {
+                    return 'Please enter the department name.'
+                } else {
+                    return true;
+                }
+            }
+        },
+    ])
+    // Await for db.addDeparment
+    await db.addDepartment(department)
+    // THEN start the application
+    startApp();
+}
 
 // Add ability to ADD role
+async function addRole() {
+    // Connect to the db for viewAllDepartments
+    const departments = await db.viewAllDepartments();
+
+    // departmentOptions is going to take the department id and display it as a name
+    // When you go to department options further below, you won't see department numbers
+    // It will say the name versus the id number.
+    const departmentOptions = departments.map(({ id, name }) => ({
+        name: name,
+        value: id,
+    }));
+
+    const role = await inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of the role?',
+            validate: answer => {
+                if (answer === '') {
+                    return 'Please enter a title!'
+                } else {
+                    return true;
+                }
+            },
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the salary of role?',
+            validate: answer => {
+                if (answer === '') {
+                    return 'Please enter a salary!'
+                } else {
+                    return true;
+                }
+            },
+        },
+        
+        // Ask which role the department belongs in
+        // Also call department options, to display the names.
+        {
+            type: 'list',
+            name: 'department_id',
+            message: 'To which department does this role belong to?',
+            choices: departmentOptions
+        },
+    ])
+    
+    await db.addRole(role)
+    // Notify user the role has been successfully added 
+    console.log('Success! A new role has been added.');
+    // Then run startApp to continue.
+    startApp();
+}
 
 // Add ability to ADD employee
 
